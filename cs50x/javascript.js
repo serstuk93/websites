f47cb7715eea4de892ae2742110641f998f3deee
serstuk <pc@pop-os.localdomain>
2022-09-17 15:48:40 +0200

    deleted search bar, stylization,delete js function

--- cs50x/javascript.js
+++ cs50x/javascript.js
@@ -1,4 +1,4 @@
-function rotatefunction(){
+function rotatefunction() {
     const rtb = document.getElementById("nvbr2");
     rtb.style.animation = "rotatelogo 4s 1";
     rtb.addEventListener("animationend", () => rtb.style.animation = "none");
