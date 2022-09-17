d3ccfb1c0d03f9f3eb603154aa12e104b408339b
serstuk <pc@pop-os.localdomain>
2022-09-17 16:48:11 +0200

    finalising

--- cs50x/javascript.js
+++ cs50x/javascript.js
@@ -1,5 +1,14 @@
 function rotatefunction() {
     const rtb = document.getElementById("nvbr2");
     rtb.style.animation = "rotatelogo 4s 1";
-    rtb.addEventListener("animationend", () => rtb.style.animation = "none");
+    rtb.addEventListener("animationend", restartfunction );
 };
+
+function restartfunction(){
+    rtb.style.animation = "none";
+};
+
+function dt() {
+    const d = new Date();
+    document.getElementById("scriptdate").innerHTML = d;
+}
